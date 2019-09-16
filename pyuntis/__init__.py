9eb46f0739b16823506e4138f5b3e79bdb6384e9
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-09-16 20:37:48 +0200

    changed plan keys to grade names

--- pyuntis/__init__.py
+++ pyuntis/__init__.py
@@ -22,7 +22,7 @@ class PyUntis:
             "date": date,
             "tag": tag,
             "informationen": [],
-            "plan": []
+            "plan": {}
         }
 
         def clean(string, info=False):
@@ -33,30 +33,27 @@ class PyUntis:
             return string
 
         for td in table.find_all("td", "list inline_header"):
-            vertretungen = {"klasse": "",
-                            "vertretungen": []}
             if valid_class.match(td.text):
-                vertretungen["klasse"] = td.text
+                plan["plan"][td.text] = []
                 for sibling in td.parent.next_siblings:
                     if sibling != "\n":
                         if sibling.find("td", "list inline_header"):
                             break
                         cols = sibling.find_all("td")
                         if len(cols) > 1:
-                            vertretungen["vertretungen"].append({
+                            plan["plan"][td.text].append({
                                 "stunde": clean(cols[0].text),
-                                "lehrer": clean(cols[1].text),
+                                "lehrerin": clean(cols[1].text),
                                 "fach": clean(cols[2].text),
                                 "raum": clean(cols[3].text),
                                 "informationen": clean(cols[4].text),
                                 "art": clean(cols[5].text)
                             })
                         else:
-                            vertretungen["vertretungen"].append({
+                            plan["plan"][td.text].append({
                                 "special_info": True,
                                 "special_info_text": clean(cols[0].text)
                             })
-                plan["plan"].append(vertretungen)
 
         infos = info_table.find_all("td", colspan="2")
 
